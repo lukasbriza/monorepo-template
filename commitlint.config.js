b69641a0fe5ef987b4e4cc0e583d4d2bd1b70f00
Lukas Briza <lukasbriza@seznam.cz>
2024-11-03 09:49:08 +0100

    feat(repo): add dockerfiles

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 export default {
   extends: '@commitlint/config-conventional',
-};
+  './*.{cjs,js}': 'eslint --cache --fix',
+  './*.{json,md,yml,yaml}': 'prettier --write',
+  './github/**/*.{yml,yaml}': 'prettier --write',
+}
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,4 @@ export default {
   './*.{cjs,js}': 'eslint --cache --fix',
   './*.{json,md,yml,yaml}': 'prettier --write',
   './github/**/*.{yml,yaml}': 'prettier --write',
-};
+}
